0ca4b504d18b19eb5ad8a43dd1f8a36a0760b816
Jayant <jayant.gupta.dln@gmail.com>
2022-07-13 18:33:50 +0530

    fragment setup complete

--- app/src/main/java/com/jayant/mycryptoworld/MainActivity.kt
+++ app/src/main/java/com/jayant/mycryptoworld/MainActivity.kt
@@ -2,13 +2,23 @@ package com.jayant.mycryptoworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.jayant.mycryptoworld.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container)
+        val navController = navHostFragment!!.findNavController()
 
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
 
 
     }
